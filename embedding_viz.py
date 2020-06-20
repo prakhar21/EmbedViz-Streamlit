d918119bf45a9fcdc10faefddf83fb41c72e6f9d
Prakhar Mishra <pmprakhargenius@gmail.com>
2020-06-20 20:18:19 +0530

    Update embedding_viz.py

--- embedding_viz.py
+++ embedding_viz.py
@@ -4,6 +4,7 @@
 
 import streamlit as st
 import time
+import argparse
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
@@ -11,14 +12,15 @@ from plotly.graph_objs import *
 from sklearn.decomposition import PCA
 import numpy as np
 from PIL import Image
-df = px.data.gapminder()
 
-# st.title("Embedding Visualizer")
-st.markdown("####  Embeddings explorer with different Reduction Schemes and Search Functionality")
-
-# feature image
-image = Image.open('feature.png')
-st.sidebar.image(image, use_column_width=True)
+def display_props():
+	# header
+	st.markdown("####  Embeddings explorer with different Reduction Schemes and Search Functionality")
+	# feature image
+	image = Image.open('feature.png')
+	st.sidebar.image(image, use_column_width=True)
+	return
+display_props()
 
 ## embeddings options
 embeddings = ("Word2Vec 1k", "GloVe 1k")
@@ -46,14 +48,20 @@ else:
 	labels = [d[0] for d in data]
 	data = np.array([d[1:] for d in data])
 	
-
 ## dimension reductions
-reductions = ("PCA", "TSNE")
-options = list(range(len(reductions)))
-reductions_type = st.sidebar.selectbox("Select Dim. Reduction", options, format_func=lambda x: reductions[x])
+def display_reductions():
+	reductions = ("PCA", "TSNE")
+	options = list(range(len(reductions)))
+	reductions_type = st.sidebar.selectbox("Select Dim. Reduction", options, format_func=lambda x: reductions[x])
+	return reductions_type
+reductions_type = display_reductions()
 
 # no. dimensions
-dim = st.sidebar.radio("Dimensions", ("2-D", "3-D"))	
+def display_dimensions():
+	dims = ("2-D", "3-D")
+	dim = st.sidebar.radio("Dimensions", dims)	
+	return dim
+dim = display_dimensions()
 
 def plot_2D(data, labels, need_labels, search=None):
 	sizes = [5]*len(labels)
@@ -103,10 +111,16 @@ def plot_3D(data, labels, need_labels, search=None):
 	return fig
 
 # search
-search_for = st.sidebar.text_input("Word Lookup", "")
+def display_search():
+	search_for = st.sidebar.text_input("Word Lookup", "")
+	return search_for
+search_for = display_search()
 
 #labels check
-need_labels = st.sidebar.checkbox("Display Labels", value=True)
+def display_labels():
+	need_labels = st.sidebar.checkbox("Display Labels", value=True)
+	return need_labels
+need_labels = display_labels()
 
 def render_plot(fig):
 	fig.update_layout(margin={"r":50,"t":100,"l":0,"b":0}, height=750, width=850)
